b806469627e4a17e5a74aa976a7c4211d3a54f06
ariel.plasencia <arielplasencia00@gmail.com>
2022-02-19 20:34:54 -0500

    add solution of problem Ciclo costo negativo I in csharp

---
+++ codes/CicloCostoNegativoI.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CicloCostoMinimoI
+{
+    class Program
+    {
+        public static int[] d;
+        public static int[] pi;
+
+        static void inicializar(List<Tuple<int, int>>[] grafo, int inicio)
+        {
+            d = new int[grafo.Length];
+            pi = new int[grafo.Length];
+
+            for (int i = 0; i < grafo.Length; i++)
+            {
+                d[i] = 100000000;
+                pi[i] = -1;
+            }
+
+            d[inicio] = 0;
+        }
+
+        static bool BellmanFord(List<Tuple<int, int>>[] grafo, int inicio)
+        {
+            inicializar(grafo, inicio);
+
+            for (int i = 0; i < grafo.Length - 1; i++)
+            {
+                for (int j = 0; j < grafo.Length; j++)
+                {
+                    for (int k = 0; k < grafo[j].Count; k++)
+                    {
+                        Relax(j, grafo[j][k].Item1, grafo[j][k].Item2);
+                    }
+                }
+            }
+
+            for (int i = 0; i < grafo.Length; i++)
+            {
+                for (int j = 0; j < grafo[i].Count; j++)
+                {
+                    if (d[grafo[i][j].Item1] > d[i] + grafo[i][j].Item2)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        static void Relax(int u, int v, int w)
+        {
+            if (d[v] > d[u] + w)
+            {
+                d[v] = d[u] + w;
+                pi[v] = u;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string[] linea = Console.ReadLine().Split();
+            int vertices = int.Parse(linea[0]);
+            int aristas = int.Parse(linea[1]);
+
+            List<Tuple<int, int>>[] grafo = new List<Tuple<int, int>>[vertices];
+            
+            for (int i = 0; i < vertices; i++)
+            {
+                grafo[i] = new List<Tuple<int, int>>();
+            }
+
+            for (int i = 0; i < aristas; i++)
+            {
+                linea = Console.ReadLine().Split();
+                int a = int.Parse(linea[0]) - 1;
+                int b = int.Parse(linea[1]) - 1;
+                int peso = int.Parse(linea[2]);
+
+                grafo[a].Add(new Tuple<int, int>(b, peso));
+            }
+
+            if (!BellmanFord(grafo, 0)) 
+                Console.WriteLine("SI");
+            else
+                Console.WriteLine("NO");
+        }
+    }
+}
